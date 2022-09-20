b8e30fa7e2aadf8d9104efe368b242c0a4ef786a
KenjiWorkES <lucas.kenjies@gmail.com>
2022-09-20 17:29:32 -0300

    add theme file with colors

---
+++ src/themes/index.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    light: '#FAFAFA',
+    dark: '#1D2A30',
+    primary300: '#E9FAF2',
+    primary400: '#D9EDDF',
+    primary500: '#51DE9A',
+    primary600: '#9CB0A4',
+    primary700: '#699B7F',
+  },
+};
